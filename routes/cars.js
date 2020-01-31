259698bd4be8b646903f28612e81b5f2eb5853cd
Kevin Pettinger <kevinpettinger@gmail.com>
2020-01-31 00:51:01 -0800

    added 2 GETs All and by id

--- routes/cars.js
+++ routes/cars.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const cars = require('../data/cars');
 const router = express.Router();
+
+router.get('/', function (request, response, next) {
+    response.send(cars);
+});
+
+router.get('/:id?', function (request, response, next ) {
+    cars.forEach((car) => {
+        if (car.id == request.params.id) {
+            response.send(car);
+        }
+    });
+
+});
+
 
 module.exports = router;
