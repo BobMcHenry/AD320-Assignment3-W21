348dec4ef0a6ca17be89ac51204ae1f60ee325ae
Deny Lie <Den@Denys-MacBook-Pro.local>
2020-02-04 15:01:02 -0800

    added get specific app with ID

--- routes/apps.js
+++ routes/apps.js
@@ -5,13 +5,28 @@
 
 /* --------GET all apps info-------------*/
 
-    router.get('/', function(req, res) {
+    router.get('/', function(req, res) {// this router will run if no ID is specified
         res.json(dataAppsJson);
-    });// this router will run if no ID is specified
+    });
 
 /* --------GET specific APP with ID------*/ 
 
-    //router.get()
+    router.get('/:id', function(req, res){
+        const found = dataAppsJson.some( dataAppsJson => dataAppsJson.id === parseInt(req.params.id)); // returns true when ID matches
+
+        if(found){
+            res.json(dataAppsJson.filter(dataAppsJson => dataAppsJson.id === parseInt(req.params.id)));
+            // function (dataAppsJson) {
+            //     dataAppsJson.id === parseInt(req.params.id)
+            // }
+        } else {
+            res.status(400).json({msg: `NO APPS with id:${req.params.id} FOUND`});
+        }
+
+                
+                
+          
+    });
 
 /*below line is to export this file with name ROUTER
     so other file can import like line 3*/
