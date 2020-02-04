a306a37ea2137bb07a8de076d4753ddc04013ea6
Deny Lie <Den@Denys-MacBook-Pro.local>
2020-02-04 11:43:42 -0800

    Added simple Get Request along with res.send

--- routes/apps.js
+++ routes/apps.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+/* simple GET request. */
+router.get('/', function(req, res, next) {
+    res.send("I am implementing in routes/apps.js \n" + new Date().toISOString());
+  });
+
 module.exports = router;
