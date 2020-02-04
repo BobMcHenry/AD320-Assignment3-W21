2286d6be8111e567b3e77ec4b2dca4b015942321
Deny Lie <Den@Denys-MacBook-Pro.local>
2020-02-04 15:00:07 -0800

    Added comment in router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan'); //--middleware
 
+//---Import all router
 const indexRouter = require('./routes/index');
 const colorsRouter = require('./routes/colors');
 const carsRouter = require('./routes/cars');
