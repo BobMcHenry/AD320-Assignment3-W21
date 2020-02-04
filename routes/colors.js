c40c14b436c78d3abcf5a3abed24b75e58b5e4a4
Larissa <lyaholkovsky@gmail.com>
2020-02-04 15:01:14 -0800

    added colors route gets

--- routes/colors.js
+++ routes/colors.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const colors = require('../data/colors');
+
+router.get('/', (req, res, next) => { 
+	res.json(colors)
+});
+
+router.get('/:id', (req, res) => {
+	const found = colors.some(colors => colors.id === parseInt(req.params.id));
+	if (found) {
+		res.json(colors.filter(colors => colors.id === parseInt(req.params.id)));
+	} else {
+		res.status(400).json({ msg: `No color found with id ${req.params.id}`})
+	}
+});
 
 module.exports = router;
