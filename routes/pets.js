78184636513b53566ad5eca97dd22abab983cd99
Alexander Sutherland <alexandernsutherland@gmail.com>
2020-02-05 08:02:44 -0800

    Initial commit of pets.js with some test routing.

--- routes/pets.js
+++ routes/pets.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = router;
+
+router.get('/', function (req, res) {
+    res.send('hello world')})
+
+    router.get('/1', function (req, res) {
+        res.send('hello world 2!')})
