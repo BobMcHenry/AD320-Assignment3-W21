a501d803d5e190784024e47173e3bd2ec71f69d0
Gourova <Natalia.Gourova1@T-Mobile.com>
2020-02-04 21:38:29 -0800

    [Natalia Gourova] Add colors fetching to color.js

--- routes/colors.js
+++ routes/colors.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router();
+// fetches all colors, returns as json array
+const colors = require('../data/colors.json');
 
+router.get('/',(req, res)=>{
+    res.json(colors);
+});
+//fetches one color, returns as json object
+router.get('/:id',(req,res)=>{
+    const color = colors.find(
+        function(colour) { return colour.id === parseInt(req.params.id); }
+    );
+    if (!color) {
+        res.status(404).send('No such color found');
+    }
+    res.json(color);
+});
 module.exports = router;
