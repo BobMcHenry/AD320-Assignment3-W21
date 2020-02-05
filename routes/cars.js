9c8ccd88fa2b69cd47ee8300a9b4282949bacdf0
StopMakingUsernamesUnique <kfmc2001@gmail.com>
2020-02-04 17:51:44 -0800

    bugtest

--- routes/cars.js
+++ routes/cars.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = router;
+
+router.get('/', (req, res) => {res.json(cars)
+});
+
+router.get('/:id', function(req, res, next) {res.json(cars.find(cars => cars.id === parseInt(req.params.id)))
+});
+
+  module.exports = router;
