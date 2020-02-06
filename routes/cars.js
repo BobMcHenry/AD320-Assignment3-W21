96a02c27b4fa1ffcd3a16316097b4beab05bc4b9
Natnael Bekele <44041437+natni@users.noreply.github.com>
2020-02-05 22:12:34 -0800

    added get all cars and by id

--- routes/cars.js
+++ routes/cars.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const NateCars = require('../data/cars.json');
+//Gets list of all cars
+router.get('/', (req, res) => {
+    res.json(NateCars);
+});
+//Gets car id
+router.get('/:id', (req, res) => {
+    const cars = NateCars.find(car => car.id === parseInt(req.params.id));
+    res.send(cars);
+});
+
 module.exports = router;
