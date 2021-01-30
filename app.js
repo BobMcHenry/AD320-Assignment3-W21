59d0e93c52eebd2e514a3ac36cae90b7570ec2db
ZenSorcere <zen.sorcere@gmail.com>
2021-01-30 12:41:22 -0800

    updates pets route for GET paths tweaks app js

--- app.js
+++ app.js
@@ -38,8 +38,9 @@ app.use(function(err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
+    // (Mike) with no deault search engine, had to change render to json
     res.status(err.status || 500);
-    res.render('error');
+    res.json('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* simple GET request. */
 router.get('/', function(req, res, next) {
   res.send("It's working, yo!!\n" + new Date().toISOString());
 });
+
+
+
+
 
 module.exports = router;
