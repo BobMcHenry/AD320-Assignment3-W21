ab21fc232f19cda6c185e3fd3c31fb50f18cc77f
CyrusSassan <CyrusSassan@users.noreply.github.com>
2021-01-31 14:08:58 -0800

    Cyrus First commit

--- routes/cars.js
+++ routes/cars.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = router;
+module.exports = router;//Json file path
+const cars = require('../data/cars.json');
+
+//Get cars objects
+router.get('/', function (req, res, next) {
+    res.json(cars);
+  });
+  
+  //Get object from id
+  router.get('/:id', function(req, res, next) {
+    const carId = Number(req.params.id);
+    const getCar = cars.find((cars) => cars.id === carId);
+  
+    //Error handler
+    if (!getCar) {
+        res.json(getCar);
+      res.status(200).send;
+    } else {
+      res.json(getCar);
+    }
+  });
