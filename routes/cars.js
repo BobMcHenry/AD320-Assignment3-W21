2d3f0b8d38a5f29c512c1936b96b8ca17d3af8b6
Hanna-N9 <hannyhabte@yahoo.com>
2021-02-02 22:51:34 -0800

    Adding to route cars.js

--- routes/cars.js
+++ routes/cars.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+//Getting information from data/cars.json
+const cars = require('../data/cars.json');
+
+//Uses res.json to send json and returns it as json
+router.get('/', function(req, res, next) {
+    res.json(cars);  //as json array hence res.json
+  });
+
+//GET request that responds with single id entry.
+router.get('/:id', function(req, res) {  //Get route
+  let reqID = req.params.id;
+  const check = cars.find(cars => cars.id == reqID); 
+
+  //Checking the id along with if blank
+    if(!check) {
+      res.status(200).json({});
+    }  
+    else{
+      res.send(check);
+    }
+  
+    });
+
+
 module.exports = router;
