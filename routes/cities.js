3c9090ad941fd7f82429f5b300810fbaadd7c920
Fikirte Mulugeta <fikirliya@gmail.com>
2021-02-03 19:26:16 -0800

    routes/cities.js

--- routes/cities.js
+++ routes/cities.js
@@ -1,39 +1,29 @@
 const express = require('express');
-const app = require('../app');
 const router = express.Router();
 
-/* router.get('/', (request, response,) => {
-    return Cities.find({}).lean()
-    .then((Cities) => {
-      console.log(Cities); 
-      
-    }) 
-    .catch(err => console.log(err));
-   
-    
-  }); */
-
-  const cities = require('../data/cities.json');
+//path for data.json file
+ const cities = require('../data/cities.json');
 
   console.log('Server started')
-  //
-  router.get('/', function (req, res,next){
-      res.json(cities);
-  })
 
-  //
+  //call all records from json file
+  router.get('/', function (req, res, next){
+      res.json(cities);
+  });
+
+   //call one of the cities from the json file
   router.get('/:id', function (req, res, next) {
 
-    const findCitiesId = cities.find((city) => city.id === parseInt(req.params.id));
+    const Cities = cities.find((city) => city.id === parseInt(req.params.id));
 
-    if (findCitiesId){
-        res.json(findCitiesId);
+    if (Cities){
+        res.json(Cities);
 
     } else {
-
-        res.sendStatus(200);
+        res.status(200).json({});
     
-    }
+    } 
+   
   });
 
 module.exports = router;
