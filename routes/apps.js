42597664969cf7a53faf746cc68faaa2e70e6e2a
[daniel22] <[ethiosunshine5628@gmail.com]>
2021-02-04 17:23:08 -0800

    My commit for the changes

--- routes/apps.js
+++ routes/apps.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
+let myGetHandler = function(req, res, next){
+    let myData = require ("../data/apps.json");
+    res.send(myData)
+}
+
+router.get('/', myGetHandler);
+
+
+
+
+
+
+
+
 
 module.exports = router;
