2d87947cb0562f696adbca7bf606e1af133e2695
xnm23 <eyerusalemdani@gmail.com>
2021-02-18 10:46:48 -0800

    new users data

--- routes/apps.js
+++ routes/apps.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+// get all the data
 let myGetHandler = function(req, res, next){
     let myData = require ("../data/apps.json");
     res.send(myData)
 }
 
 router.get('/', myGetHandler);
+
+// get the data with ID
 
 
 
